c9f2aff4f991d6f10f45bfcc6d463172ead8c193
pilpod <giacomo.corso@gmail.com>
2021-07-16 20:52:48 +0200

    option two with comments

--- src/RepeatedString.php
+++ src/RepeatedString.php
@@ -8,22 +8,26 @@ class RepeatedString {
 
     public function repeat(string $s,int $n) : int
     {
-        Define('CHAR', 'a');
-    
-        if (!gettype($n) === 'integer' || !gettype($s) === 'string' || !$s || $n <= 0) {
-            return 0;
-        }
-        if (!strcasecmp($s, CHAR)) {
-            return $n;
-        }
-        
         $len = strlen($s);
-        
-        $occurrences = substr_count($s, CHAR);
-        $occurrences *= floor($n / $len);
-        $occurrences += substr_count(substr($s, 0, $n % $len), CHAR);
-        
-        return $occurrences;
+        print_r($len); // longitud string
+
+        $occurence = substr_count($s, "a");
+        print_r($occurence); // cantidad de a
+
+        $mul = floor ($n / $len);
+        print_r($mul); // Dividimos para saber cuantas veces se repite el string entero redondeando  la baja
+
+        $reminder = $n % $len;
+        print_r($reminder); // Calculamos el resto para saber cuantas letras tiene la última secuencia
+
+        $su = substr($s,0,$reminder);
+        print_r($su); // obtenemos los caracteres de la última secuencia
+
+        // Cantidad de a por secuencia repetida más cantidad de a en la última secuencia
+        $result = ($occurence * $mul) + substr_count($su,"a");
+        print_r($result);
+
+        return $result;
     }
 
 }
